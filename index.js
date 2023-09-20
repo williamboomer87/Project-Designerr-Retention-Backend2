cacb5b9e136f9c7b928d5a7c354c7d0d089b024e
vimuths123 <vimuths@yahoo.com>
2023-09-20 17:00:15 +0530

    New

---
+++ index.js
@@ -0,0 +1,29 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+  });
+});
+
+app.get("/path", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+// module.exports.handler = serverless(app);
+
+const port = 8080;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
