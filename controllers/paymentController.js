6803dbaa3540cfdac996e7e7e468f2065b98a9f7
vimuths123 <vimuths@yahoo.com>
2023-09-21 00:43:03 +0530

    New changes

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -2,6 +2,8 @@
 require('dotenv').config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
+const endpointSecret = 'we_1NsVeeJYVRzxBOJIbpbIxvCG';
+
 const calculateOrderAmount = (items) => {
   return 1400;
 };
@@ -25,15 +27,51 @@ const createPayment = async (req, res) => {
 
 const getPaymentData = async (req, res) => {
 
-  let event = req.body;
+  // let event = req.body;
 
-  console.log('-----------------')
-  console.log('Payment done')
-  console.log('-----------------')
+  // console.log('-----------------')
+  // console.log('Payment done')
+  // console.log('-----------------')
 
-  
 
-  return res.status(200).json({ success: true });
+
+  // return res.status(200).json({ success: true });
+
+  let event = request.body;
+  if (endpointSecret) {
+    const signature = request.headers['stripe-signature'];
+    try {
+      event = stripe.webhooks.constructEvent(
+        request.body,
+        signature,
+        endpointSecret
+      );
+    } catch (err) {
+      console.log(`Webhook signature verification failed.`, err.message);
+      return response.sendStatus(400);
+    }
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case 'payment_intent.succeeded':
+      const paymentIntent = event.data.object;
+      console.log(`PaymentIntent for ${paymentIntent.amount} was successful!`);
+      // Then define and call a method to handle the successful payment intent.
+      // handlePaymentIntentSucceeded(paymentIntent);
+      break;
+    case 'payment_method.attached':
+      const paymentMethod = event.data.object;
+      // Then define and call a method to handle the successful attachment of a PaymentMethod.
+      // handlePaymentMethodAttached(paymentMethod);
+      break;
+    default:
+      // Unexpected event type
+      console.log(`Unhandled event type ${event.type}.`);
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  response.send();
 }
 
 
